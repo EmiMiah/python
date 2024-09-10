fdc687ed415c511d17fde847b50cf5b9ab0459bf
Emi <144714919+EmiMiah@users.noreply.github.com>
2024-09-10 11:41:25 -0400

    Create CFU1.py

---
+++ unit1/CFU1.py
@@ -0,0 +1,39 @@
+# Monday 9/9/2024
+#Declaring a string variable
+course ="Programming & Prototyping"
+fname ="Emirah" # string variable
+age = 55 # Number variable
+fact = True # Boolean Variable
+#input and out functions
+
+# to Output use key word print lowercase
+
+print(13) #will print number
+print("hello") # will print a string
+print(True) # will print boolean condition
+
+print(course) #will print the value of the variable
+
+print("Hello " + fname + "!")
+
+#to input use the keyword input in lowercase
+user_name = input("What's your name?")
+
+print("Nice to meet you" + user_name)
+
+print(user_name, "you are register in", course)
+
+#Monday 9-9-2024 pd 5-6
+
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+print("Nice to meet you" + " " + first_name + " " + last_name + "!")
+
+#9-10-24
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+print("Nice to meet you" + " " + first_name + " " + last_name + "!")
+fav_color = input("What is your favorite color?")
+print("That's wonderful," + fav_color + " " + "is also one of my many favorites!")
+sub_ject = input("What subject are you learning right now?")
+print("Oh, thats great! I hope you enjoy"+ " " + sub_ject + "!")
