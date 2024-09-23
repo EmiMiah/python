36552cd1f1203c7889687a1ab4feefb09af47b32
Emi <144714919+EmiMiah@users.noreply.github.com>
2024-09-23 12:49:45 -0400

    Update practice.py

--- unit 2/practice.py
+++ unit 2/practice.py
@@ -18,7 +18,28 @@ print("the answer is" + str(sol))
 
 
 import math
-r= int(input("What is the radius of you circle?"))
+r= int(input("What is the radius of your circle?"))
 c= 2*r*math.pi
 a= math.pi*r**2
 print("c=",c," a=",a)
+
+
+import math
+r= float(input("What is the radius of your circle?"))
+h= int(input("What is the height of your circle?"))
+c= 2*r*math.pi
+a= math.pi*r**2*h
+print("c=",c," a=",a)
+
+import math
+name = input("What is your name?")
+a = int(input("What is a?"))
+b = int(input("What is b?"))
+c = int(input("What is c?"))
+quad = (b**2-4*a*c)
+posX = (-b+math.sqrt(quad))/(2*a)
+negX = (-b-math.sqrt(quad))/(2*a)
+
+
+print(posX)
+print(negX)
