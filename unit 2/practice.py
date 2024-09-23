76d5cdbf8903d151096a5d737052b8682b3b4f3c
Emi <144714919+EmiMiah@users.noreply.github.com>
2024-09-23 11:53:20 -0400

    Create practice.py

---
+++ unit 2/practice.py
@@ -0,0 +1,10 @@
+'''
+This program calculates the perimeter of a rectangle using input from the user.
+'''
+#Getting the legth from the user
+length = int(input("What is the length of the rectangle?"))
+width = int(input("What is the width of the rectangle?"))
+
+perimeter= 2* (length+width)
+
+print ("The perimeter is" + str (perimeter))
